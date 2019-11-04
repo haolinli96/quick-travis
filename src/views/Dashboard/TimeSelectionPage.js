9aab36fea04ad799c689a2e0c81d31e752635622
haolinli96 <lihaolin@u.northwestern.edu>
2019-11-04 10:33:38 -0600

    try deploy1

--- src/views/Dashboard/TimeSelectionPage.js
+++ src/views/Dashboard/TimeSelectionPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
 import Calendar from '../UserProfile/Calendar.jsx';
